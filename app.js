c25a265d1e3761865ab13516d3deb77dbc091678
Gguife <gguife747@gmail.com>
2025-01-16 01:20:33 -0300

    feat: Criado funcoes para adiconar, listar e sortear amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,42 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let input = document.querySelector('#amigo');
+let lista = document.querySelector('#listaAmigos');
+let amigos = [];
+
+function adicionarAmigo() {
+  let nome = input.value;
+
+  if(nome == '') {
+    alert('Por favor, digite um nome válido!');
+  }
+
+  amigos.push(nome);
+  input.value = ''; 
+
+  listaDeAmigos();
+}
+
+
+function listaDeAmigos() {
+  lista.innerHTML = '';
+
+  for(let i = 0; i < amigos.length; i++){
+    let li = document.createElement('li');
+    li.textContent = amigos[i];
+    lista.appendChild(li);
+  }
+}
+
+
+function sortearAmigo() {
+  if(amigos.length == 0) {
+    alert('Adicione amigos para realizar o sorteio!');
+  }
+
+  let indiceAmigo = Math.floor(Math.random() * amigos.length);  
+  let amigoSorteado = amigos[indiceAmigo];
+
+  document.querySelector('#resultado').innerHTML = `O amigo sorteado é: ${amigoSorteado}`;
+
+  amigos = [];
+  lista.innerHTML = '';
+}
